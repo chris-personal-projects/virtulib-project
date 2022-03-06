ea9f75e4b9172911a1eb4b51591e4997d009b9c3
Chris Chen <chrischentan@hotmail.com>
2022-03-06 14:30:26 -0700

    Added first prototype for checkout books overlay

---
+++ Pages/ChrisTest.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace virtulib_project.Pages
+{
+    /// <summary>
+    /// Interaction logic for ChrisTest.xaml
+    /// </summary>
+    public partial class ChrisTest : Page
+    {
+        public ChrisTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
