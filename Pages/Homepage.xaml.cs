6f3403be4732f2bbd9ee1ffc01355f00c09a3f2e
Zelin Zhou <zelin.zhou1@ucalgary.ca>
2022-03-07 14:46:57 -0700

    CheckoutPage created

---
+++ Pages/Homepage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace virtulib_project.Pages
+{
+    /// <summary>
+    /// Interaction logic for Homepage.xaml
+    /// </summary>
+    public partial class Homepage : Page
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/Browse.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Installation Instructions
+To install the packages used in this project, right-click the project and select "Manage NuGet Packages". 
+The package manager will then look at the packages in `packages.config` to install.
