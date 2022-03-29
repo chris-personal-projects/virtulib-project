e855d0886097ddfe868ff773e78a25385ee44554
Chris Chen <chrischentan@hotmail.com>
2022-03-29 17:12:38 -0600

    WIP JSON data

--- Models/MainViewModel.cs
+++ Models/MainViewModel.cs
@@ -6,16 +6,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace virtulib_project.Models
 {
     public class MainViewModel : ObservableObject
     {
+        private string dataPath = @"/data/bookdata.json";
+        private VirtulibBookModel[] bookList;
+
         public DialogViewModel MainDialog { get; private set; }
 
         public MainViewModel()
         {
             MainDialog = new DialogViewModel();
+            // ParseBookData();
+            
         }
 
         public void SetDialog(object dialog)
@@ -23,5 +30,19 @@ namespace virtulib_project.Models
             MainDialog.DialogObject = dialog;
             MainDialog.IsShow = !MainDialog.IsShow; // Toggle the Dialog on or off
         }
+
+        //private void ParseBookData()
+        //{
+        //    using(StreamReader file = File.OpenText(dataPath))
+        //    {
+        //        string jsonstring = file.ReadToEnd();
+        //        JObject jsonObj = JObject.Parse(jsonstring);
+        //        var jsonArray = JArray.Parse(jsonObj["virtulib_data"].ToString());
+        //        bookList = jsonArray.ToObject<List<VirtulibBookModel>>().ToArray();
+
+        //        Console.WriteLine(bookList[0]);
+        //    }
+
+        //}
     }
 }
