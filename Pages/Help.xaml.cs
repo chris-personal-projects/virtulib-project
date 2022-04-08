642ebe89c7203cc820dc62f9aaa81638bace8394
klarshin <ksl37301@yahoo.ca>
2022-04-08 16:33:51 -0600

    added beginning of help system

--- Pages/Help.xaml.cs
+++ Pages/Help.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using virtulib_project.UserControls;
 
 namespace virtulib_project.Pages
 {
@@ -23,6 +24,26 @@ namespace virtulib_project.Pages
         public Help()
         {
             InitializeComponent();
+            addHelpItems();
+        }
+
+        public void addHelpItems() {
+            HelpItem helpItem;
+            Border helpBorder;
+            string[] categoryList = { "How to Search", "How to Return", "How to Sign Up", "Getting Started", "How to Change Settings", "Keeping Track of Due Dates", "Placing Holds", "Using Filters", "Tracking Availability" };
+            for (int i = 0; i < 10; i++) {
+                if (i > 8)
+                {
+                    helpItem = new HelpItem(categoryList[i - 9]);
+                }
+                else {
+                    helpItem = new HelpItem(categoryList[i]);
+                }
+                
+                helpBorder = new Border();
+                helpBorder.Child = helpItem;
+                HelpArticleGrid.Children.Add(helpBorder);
+            }
         }
     }
 }
